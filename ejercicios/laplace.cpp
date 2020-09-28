45faa91506a7eef040c44b606c24397ed4d00154
diego ordóñez <diordonezp@unal.edu.co>
2020-09-28 12:08:01 -0500

    solución de la ecuación de laplace para una caja de lados a, con potencial nulo en todas las caras menos la inferior y superior de valor v

---
+++ ejercicios/laplace.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cmath>
+
+double phi(double x,double y,double z,double a,double v);
+double a_nm(int n, int m,double a, double x, double y, double z);
+
+int main(void)
+{
+    std::cout.precision(15);
+    std::cout.setf(std::ios::scientific);
+
+    double a=1.0;
+    double v=2.0;
+    
+    for(double x=0.0;x<=a;x+=0.001){
+        std::cout<<x<<"\t"<<phi(0.5,0.5,x,a,v)<<"\n";
+    }
+    
+    return 0;
+}
+
+double a_nm(int n, int m,double a, double x, double y, double z)
+{
+    double k_nm=M_PI*std::hypot(n,m)/a;
+    double term=((std::sinh(k_nm*z)+std::sinh(k_nm*(a-z)))/(n*m*std::sinh(k_nm*a)))*std::sin(n*M_PI*x/a)*std::sin(m*M_PI*y/a);
+    
+    return term;
+}
+
+double phi(double x,double y,double z,double a,double v)
+{
+    double sum=0.0;
+
+    for(int n=1;n<=108;n+=2){
+        for(int m=1;m<=200;m+=2){
+            sum+=a_nm(n,m,a,x,y,z);
+        }
+    }
+    sum=16*v/std::pow(M_PI,2)*sum;
+    
+    return sum;
+}
