c03630852f7cd4698af1f661f63136d843829606
diego ordóñez <diordonezp@unal.edu.co>
2020-11-15 19:49:02 -0500

    1 parallel version

--- ejercicios/openmp/parallel_integral.cpp
+++ ejercicios/openmp/parallel_integral.cpp
@@ -1,13 +1,22 @@
 #include<iostream>
 #include<cmath>
+#include<chrono>
 
 double fun(double x);
-double integral(double a, double b, double h);
+double integral(double a, double b,int h,int n);
 
-int main(void)
+int main(int argc,char **argv)
 {
-	std::cout << integral(0,10,10000000)
-	<<std::endl;
+    std::cout.precision(15);std::cout.setf(std::ios::scientific);
+    int num_threads=std::atoi(argv[1]);
+
+    auto start=std::chrono::steady_clock::now();
+    double I=integral(0,10,100000000,num_threads);
+    auto end=std::chrono::steady_clock::now();
+
+    double t=std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()/1000.0;
+    
+	std::cout<<num_threads<<"\t"<<I<<"\t"<<t<<"\n";
 	
 	return 0;
 }
@@ -17,12 +26,13 @@ double fun(double x)
 	return std::pow(x,2);
 }
 
-double integral(double a, double b, double h)
+double integral(double a,double b,int h,int n)
 {
 	double sum=0;
 	double dx=(b-a)/h;
 
-	for(int i=1; i<=h; i++){
+#pragma omp parallel for num_threads (n) reduction(+:sum)
+	for(int i=1; i<h+1; i++){
 		sum+=fun(a+i*dx);
 	}
 	return sum*dx;
